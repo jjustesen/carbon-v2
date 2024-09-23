5b56ff199f345b64b8fdf440ef7d11169e9a8133
jjustesen <johannesjustesen99@gmail.com>
2024-09-23 01:49:40 -0300

    Refactor tsconfig.json to add "dom" library and enable esModuleInterop and skipLibCheck Update package.json name and displayName to "vscode-carbon" and "Carbon code" respectively Delete extension.test.js and extension.test.js.map files Add MIT License Delete graph-visualization.js.map file Refactor fileAnalyzer.js, graphVisualization.js, treeViewProvider.js, and extension.js to use ES modules

--- out/importExportParser.js
+++ out/importExportParser.js
@@ -1,8 +1,30 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.parseExports = parseExports;
-exports.parseImports = parseImports;
-const ts = require("typescript");
+exports.parseImports = exports.parseExports = void 0;
+const ts = __importStar(require("typescript"));
 function parseExports(content) {
     const sourceFile = ts.createSourceFile("temp.ts", content, ts.ScriptTarget.Latest, true);
     const exports = {};
@@ -55,6 +77,7 @@ function parseExports(content) {
     visit(sourceFile);
     return exports;
 }
+exports.parseExports = parseExports;
 function parseImports(content) {
     const sourceFile = ts.createSourceFile("temp.ts", content, ts.ScriptTarget.Latest, true);
     const imports = [];
@@ -81,4 +104,5 @@ function parseImports(content) {
     visit(sourceFile);
     return imports;
 }
+exports.parseImports = parseImports;
 //# sourceMappingURL=importExportParser.js.map
